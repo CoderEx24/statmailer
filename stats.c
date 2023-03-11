807dfd32410c69fb32b50001f0d8fc397d5486b8
CoderEx24 <coderex24@protonmail.com>
2023-03-11 21:24:47 +0200

    added forgotten fclose calls

--- stats.c
+++ stats.c
@@ -15,6 +15,7 @@ const char* get_mem_info()
     const char* buf = (const char*) malloc(4 * 1024* sizeof(char));
     fread((void*) buf, sizeof(char), 4 * 1024, mem_file);
 
+    fclose(mem_file);
     return buf;
 }
 
@@ -31,38 +32,41 @@ const char* get_cpu_info()
     const char* buf = (const char*) malloc(20 * 1024 * sizeof(char));
     fread((void*) buf, sizeof(char), 20 * 1024, cpu_file);
 
+    fclose(cpu_file);
     return buf;
 }
 
 const char* get_net_info()
 {
-    FILE *cpu_file = fopen("/proc/net/sockstat", "r");
+    FILE *net_file = fopen("/proc/net/sockstat", "r");
 
-    if (!cpu_file)
+    if (!net_file)
     {
         fprintf(stderr, "failed to open /proc/net/sockstat");
         return NULL;
     }
 
     const char* buf = (const char*) malloc(4 * 1024 * sizeof(char));
-    fread((void*) buf, sizeof(char), 4 * 1024, cpu_file);
+    fread((void*) buf, sizeof(char), 4 * 1024, net_file);
 
+    fclose(net_file);
     return buf;
 }
 
 const char* get_dsk_info()
 {
-    FILE *cpu_file = fopen("/proc/diskstats", "r");
+    FILE *dsk_file = fopen("/proc/diskstats", "r");
 
-    if (!cpu_file)
+    if (!dsk_file)
     {
         fprintf(stderr, "failed to open /proc/diskstats");
         return NULL;
     }
 
     const char* buf = (const char*) malloc(6 * 1024 * sizeof(char));
-    fread((void*) buf, sizeof(char), 6 * 1024, cpu_file);
+    fread((void*) buf, sizeof(char), 6 * 1024, dsk_file);
 
+    fclose(dsk_file);
     return buf;
 }
 
