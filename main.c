5927be18b2be2452cc89d6a9dca5a6fec5a4e28f
CoderEx24 <coderex24@protonmail.com>
2023-03-11 20:12:29 +0200

    implemented saving mem stats to a temp file

--- main.c
+++ main.c
@@ -2,6 +2,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "stats.h"
+
+int write_mem_file(const char* tempdir, int i, const char* buf, size_t bufsize)
+{
+    char* filename = (char*) malloc(128 * sizeof(char));
+    snprintf(filename, 128, "%s/mem%i.txt", tempdir, i);
+
+    FILE *mem_file = fopen((const char*) filename, "w");
+
+    if (!mem_file)
+        return -1;
+
+    unsigned long bytes = fwrite(buf, sizeof(char), bufsize, mem_file);
+
+    if (bytes < 0)
+        return -1;
+
+    free(filename);
+    fclose(mem_file);
+
+    return bytes;
+}
+
 int main()
 {
     pid_t child;
@@ -14,10 +37,32 @@ int main()
         exit(0);
     else
     {
-        system("sleep 10");
-        printf("I am the child\n");
-    }
+        
+        sleep(4);
 
+        char template[] = "/tmp/statmailer.XXXXXX";
+        char *dir_name = mkdtemp(template);
+        
+        if (!dir_name)
+        {
+            perror("failed to create tmpdir");
+            exit(-1);
+        }
+
+        for (int i = 0;; i ++)
+        {
+            const char* mem_info = get_mem_info();
+
+            if (write_mem_file(dir_name, i, mem_info, 4 * 1024) < 0)
+                perror("failed to write to memfile");
+
+            free((void*) mem_info);
+            mem_info = NULL;
+
+            sleep(60);
+        }
+
+    }
 
 }
 
